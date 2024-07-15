dcb5bc4120238ec6ac21c270a78918c3ae74a9cb
Duane Francis Rodrigues <Duane.Rodrigues@bbd.co.za>
2024-07-15 16:29:06 +0530

    Update V2__insert_data_add_view.sql

--- migrations/V2__insert_data_add_view.sql
+++ migrations/V2__insert_data_add_view.sql
@@ -7,19 +7,19 @@ VALUES (1,'Pune '), (2,'Cape town '),(3,'London'),(4,'Amsterdam'),(5,'Pretoria')
 INSERT INTO `availability`(`availabilityID`, `officeID`, `productID`)
 VALUES (1,3,4), (2,1,7), (3,2,2)
 
-INSERT INTO `orderstatus`(`orderStatusID`, `orderStatusValue`)
+INSERT INTO `orderStatus`(`orderStatusID`, `orderStatusValue`)
 VALUES (1,'Pending'),(2,'In progress'),(3,'Prepared'),(4,'Complete'),(5,'Cancelled');
 
-INSERT INTO `userstatus`(`userStatusID`, `statusValue`)
+INSERT INTO `userStatus`(`userStatusID`, `statusValue`)
 VALUES (1,'Active'),(2,'Inactive'),(3,'Ban');
 
-INSERT INTO `usertype`(`userTypeID`, `userTypeValue`)
+INSERT INTO `userType`(`userTypeID`, `userTypeValue`)
 VALUES (1,'Employee'),(2,'Barista');
 
-INSERT INTO `appuser`(`userID`, `firstName`, `lastName`, `officeID`, `userStatusID`, `userTypeID`) 
+INSERT INTO `appUser`(`userID`, `firstName`, `lastName`, `officeID`, `userStatusID`, `userTypeID`) 
 VALUES ('1025', 'swizel', 'antao', '2', '1', '1'), ('1056', 'duane', 'rodrigues', '5', '2', '2'), ('1025', 'sanketh', 'shenoy', '1', '3', '1');
 
-INSERT INTO `orderlist`(`orderID`, `productID`, `orderStatusID`, `quantity`, `userID`) 
+INSERT INTO `orderList`(`orderID`, `productID`, `orderStatusID`, `quantity`, `userID`) 
 VALUES ('1', '1', '2', '2', '1025'), ('2', '5', '2', '2', '1025'), ('3', '2', '1', '1', '1028');
 
 
