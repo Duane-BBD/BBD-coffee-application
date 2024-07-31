f51bc33a41481218ddc8dee1bf37414300e057ef
Sankethbbd <Sanketh.Shenoy@bbd.co.za>
2024-07-31 15:13:28 +0530

    fixed bug:added DTO obj to the query

--- Spring-Boot/src/main/java/com/bbdsoftware/coffee/repository/OrderListRepository.java
+++ Spring-Boot/src/main/java/com/bbdsoftware/coffee/repository/OrderListRepository.java
@@ -9,14 +9,11 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface OrderListRepository extends JpaRepository<OrderList, Integer> {
-    @Query("SELECT orderID, productID, quantity FROM OrderList ol WHERE ol.userID = :userID")
-    List<OrderList> findOrdersByUserId(Integer userID);
-
-    @Query("SELECT ol.orderID, p.productName, ol.quantity, ol.notes, mt.milkTypeValue\n" +
-            "FROM OrderList ol \n" +
-            "INNER JOIN Product p ON p.productID = ol.productID\n" +
-            "INNER JOIN MilkType mt ON mt.milkTypeID = ol.milkTypeID\n" +
-            "INNER JOIN OrderStatus os ON os.orderStatusID = ol.orderStatusID \n" +
+    @Query("SELECT new com.bbdsoftware.coffee.DTO.BaristaDisplayDTO(ol.orderID, p.productName, ol.quantity, ol.notes, mt.milkTypeValue) " +
+            "FROM OrderList ol " +
+            "INNER JOIN Product p ON p.productID = ol.productID " +
+            "INNER JOIN MilkType mt ON mt.milkTypeID = ol.milkTypeID " +
+            "INNER JOIN OrderStatus os ON os.orderStatusID = ol.orderStatusID " +
             "WHERE ol.office.officeID = :officeID AND os.orderStatusValue = :orderStatusValue")
     List<BaristaDisplayDTO> fetchBaristaDisplayDetails(@Param("officeID") Integer officeID, @Param("orderStatusValue") String orderStatusValue);
 }
