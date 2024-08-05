e81c702913b4578854163e0cf3ab0ec327f75fe6
Sankethbbd <Sanketh.Shenoy@bbd.co.za>
2024-08-05 16:18:08 +0530

    alloffices page

--- react/src/ui/user/components/AllOffices.js
+++ react/src/ui/user/components/AllOffices.js
@@ -1,36 +1,35 @@
 import React, { useEffect, useState } from "react";
 import FrontPage from './FrontPage';
 import { SlArrowRight } from "react-icons/sl";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import "../static/AllOffices.css";
 import Coffeebean from "../../common/images/coffeebean.png";
 import { allLocation } from "../../../services/locationService";
-import { useLocationContext } from '../../../context/LocationContext';
-
+ 
 export default function AllOffices() {
-    const { setLocation } = useLocationContext();
+    const navigate = useNavigate()
     const [offices, setOffices] = useState([]);
-
+ 
     useEffect(() => {
-        allLocation(setOffices);
-    }, []);
-
-    return (
-        <div className="container">
-            <div className="frontpage-container">
-                <FrontPage />
-            </div>
-            <div className="alloffices-container">
-                <div className='alloffices'>
-                    <h3 className="pick-location">Pick your location</h3>
+        allLocation(setOffices)
+    }, [offices])
+ 
+  return (
+    <div className="container">
+        <div className="frontpage-container">
+            <FrontPage/>
+        </div>
+        <div className="alloffices-container">
+            <div className='alloffices'>
+                <h3 className="pick-location">Pick your location</h3>
                     {offices.map((locate, index) => (
                         <div className="mapping" key={index}>
                             <Link
-                                to={`/products`}
-                                onClick={() => setLocation(locate)} // Set location on click
+                                to={`/product`}
+                                state={locate}
                                 className="location-option"
                             >
-                                <img src={Coffeebean} className="coffeebean" alt="Coffee bean" />
+                                <img src={Coffeebean} className="coffeebean" />
                                 <span>{locate.officeName}</span>
                                 <div className="arrow">
                                     <SlArrowRight />
@@ -38,8 +37,8 @@ export default function AllOffices() {
                             </Link>
                         </div>
                     ))}
-                </div>
             </div>
         </div>
-    );
+    </div>
+  )
 }
