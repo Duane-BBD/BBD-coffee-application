d0da8e085f44c2ad0b191070813a4ebf6dc6ba2d
Duane-BBD <Duane.Rodrigues@bbd.co.za>
2024-08-06 17:05:46 +0530

    Update Products

--- react/src/ui/barista/components/OrderDetails.js
+++ react/src/ui/barista/components/OrderDetails.js
@@ -1,19 +1,23 @@
 import React, { useEffect } from 'react';
 import '../static/OrderDetails.css';
 import Navbar from '../../common/components/Navbar';
-import { useFetcher, useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { cancelOrders, progressStatus } from '../../../services/updateStatusService';
+import { MdKeyboardArrowLeft } from "react-icons/md";
+
 const OrderDetails = () => {
     const location=useLocation();
-    const order = location.state || { name: '', quantity: 0 }; 
-    useEffect(()=>{
-      console.log(order,'name')  
-    },[])
+    const navigate = useNavigate();
+    const order = location.state;
+    const searchParams = new URLSearchParams(location.search);
+    let status = searchParams.get('status') || 'Pending';
+
     return (
         <div className="orderdetails-container">
             <div className="order-header">
-                <button className="back-button">&lt;</button>
-                <span className="order-number">Order number</span>
-                <span className="order-status">Pending</span>
+                <button className="back-button" onClick={() => navigate(-1)}><MdKeyboardArrowLeft/></button>
+                <span className="order-number">{order.orderID}</span>
+                <span className="order-status">{status}</span>
             </div>
 
             <div className="order-items">
@@ -30,8 +34,20 @@ const OrderDetails = () => {
             </div>
 
             <div className="order-actions">
-                <button className="decline-button">Decline order</button>
-                <button className="accept-button">Accept order</button>
+                <button 
+                    className="decline-button"
+                    onClick={() => {
+                        cancelOrders(order.orderID)
+                        navigate(`/take-orders`)
+                    }}
+                >Decline order</button>
+                <button 
+                    className="accept-button"
+                    onClick={() => {
+                        progressStatus(order.orderID)
+                        navigate(`/take-orders`)
+                    }}
+                >Accept order</button>
             </div>
             <div className='content'>
                 <Navbar />
