7bda2f679ada03eda56ae35c168a775402b74c93
Sankethbbd <Sanketh.Shenoy@bbd.co.za>
2024-08-11 19:47:35 +0530

    fixed bugs

--- react/src/ui/user/UserLanding.js
+++ react/src/ui/user/UserLanding.js
@@ -3,11 +3,10 @@ import AllOffices from './components/AllOffices';
 import Products from './components/Products';
 import ProductDetails from './components/ProductDetails';
 import CheckOut from './components/CheckOut';
-import PlaceOrder from './components/PlaceOrder';
 import { Route, Routes } from 'react-router-dom';
 import OrderHistory from './components/OrderHistory';
 import UserOrderHistory from './components/UserOrderHistory';
-import MyOrders from './components/PlaceOrder';
+import MyOrders from './components/MyOrders';
 import Missing from '../common/components/Missing';
 
 const UserLanding = () => {
@@ -17,7 +16,6 @@ const UserLanding = () => {
 		<Route path="/product" element={<Products/>} />
 		<Route path="/product-details" element={<ProductDetails/>}/>
 		<Route path="/check-out" element={<CheckOut/>}/>
-        <Route path="/place-order" element={<PlaceOrder/>}/>
         <Route path="/order-history" element={<OrderHistory/>}/>
         <Route path="/user-order-history" element={<UserOrderHistory/>}/>
         <Route path="/my-orders" element={<MyOrders/>}/>
